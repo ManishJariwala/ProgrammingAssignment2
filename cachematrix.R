e4b4629ead55a4587ee6265f8c78dad6e0c81687
Manish jariwala <manish_jariwala@yahoo.com>
2014-10-24 12:18:31 -0400

    Matrix Project

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,41 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The below program consists of two programs  - makeCacheMatrix and cacheSolve.  
+## makeCacheMatrix will be run the first time, which takes matrix as an argument
+## It will calculate inverse of matrix in a global variable m to store the value of results.
+##  cacheSolve will first check if makeCacheMatrix by calling getinverse to see if results exists,
+#   If it does not, it will calculate resolves and store in global variable m.
 
-## Write a short comment describing this function
+
+## makeCacheMatrix function provides an ability to calculate inverse matrix and store in global variable m
 
 makeCacheMatrix <- function(x = matrix()) {
+  
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(solve) m <<- solve
+  getinverse <- function() m
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 
 }
 
 
-## Write a short comment describing this function
+## cacheSolve makes call to makeCacheMatrix to determine if inverse was already calculate. If not, it calculates it for first 
+## time and stores in global variable via calling functions within makeCacheMatrix.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  m <- x$getinverse()
+  if(!is.null(m)) {cacheSolve(amatrix)
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setinverse(m)
+  m
 }
